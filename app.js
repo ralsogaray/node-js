7a5d74065e0678ed1bf65d66b6bc64fa4ce1d60d
rodrigo alsogaray <rodrigoalsogaray@MacBook-Air-de-rodrigo.local>
2020-10-20 21:46:00 -0300

    configuracion server inicial

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const server = (request, response) => {//reglas del servidor
 
 
 
-http.createServer( server ).listen(port) //funcion crear servidor, dentro de los () voy a programar las reglas del servidor. El servidor va a escuchar todas las peticiones que entren por el servidor 1000
+http.createServer( server ).listen( port ) //funcion crear servidor, dentro de los () voy a programar las reglas del servidor. El servidor va a escuchar todas las peticiones que entren por el servidor 1000
 
 
 
