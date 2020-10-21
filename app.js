ebbb26452c6ca89a929f20436dd0bba06809bbaa
rodrigo alsogaray <rodrigoalsogaray@MacBook-Air-de-rodrigo.local>
2020-10-20 21:29:30 -0300

    configuracion server inicial

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello world!")
+const http = require("http")//luego de instalar el module, deposito el modulo en la constante 'http'
+
+const port = 1000
+
+const server = (request, response) => {//reglas del servidor
+
+    response.end("Hello world!")
+}
+
+
+
+http.createServer( server ).listen(port) //funcion crear servidor, dentro de los () voy a programar las reglas del servidor. El servidor va a escuchar todas las peticiones que entren por el servidor 1000
+
+
+
