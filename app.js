fcecad2dd8a79d8b5975952294344656b292709f
rodrigo alsogaray <rodrigoalsogaray@MacBook-Air-de-rodrigo.local>
2020-10-20 22:10:05 -0300

    Sirviendo archivos estaticos

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const http = require("http")//luego de instalar el module, deposito el modulo en la constante 'http'
+const fs = require('fs')// luego de instalar el modulo fs, lo deposito en la constante --> fs = file sisteme
+
 
 const port = 1000
 
 const server = (request, response) => {//reglas del servidor
 
-    response.end("Hello world!")
+
+    fs.readFile('front/index.html', (error, file) => { // le digo que archivo leer y luego una funcion para indicarle que hacer
+        if(error){
+            response.writeHead(404, {"content-Type" : "text/plain"}) //codigo de error y digo que es texto
+            response.end("Malio Sal(sa)...")
+        } else {
+            response.writeHead(200, {"Content-Type" : "text/html"})
+            response.end(file)
+        }
+    })  
+
+    
 }
 
 
