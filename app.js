8d6e551048e0e5ed26eb1cb29e0594aed52a6cac
rodrigo alsogaray <rodrigoalsogaray@MacBook-Air-de-rodrigo.local>
2020-10-22 21:28:20 -0300

    Creando front de Formulario de contacto

--- app.js
+++ app.js
@@ -9,13 +9,24 @@ const port = 1000
 const app = express()
 
 app.listen(port)
+
+app.use( express.static( 'public' ) ) // con esto le digo q primero busque en la carpeta public y si no encuentra lo q busca ahi, buscara en app.js
+
 /* //plantilla modelo para "endpoints" de express()
 app.TIPO_HTTP("/ruta",function(request, response){ <<<-------- //anatomia modelo de como crear rutas en mi servidor con express. Esta el tipo de peticion y la ruta con la cual se va a acceder al codigo
 
 })*/
 
 app.get("/contacto", function(request, response){  //anatomia de como crear rutas en mi servidor con express. Esta el tipo de peticion y la ruta con la cual se va a acceder al codigo
-    response.end(`DESDE ACA VAMOS A CONTACTARNOS`)
+    //response.end(`DESDE ACA VAMOS A CONTACTARNOS`)
+
+
+})
+
+
+ //plantilla modelo para "endpoints" de express()
+app.post("/enviar",function(request, response){ //<<<-------- anatomia modelo de como crear rutas en mi servidor con express. Esta el tipo de peticion y la ruta con la cual se va a acceder al codigo
+    response.end('desde aca vamos a hacer algo loco')
 })
 
 
