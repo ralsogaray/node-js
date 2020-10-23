136e30a5258be98950d8c4b7b19ce30ad1bca951
rodrigo alsogaray <rodrigoalsogaray@MacBook-Air-de-rodrigo.local>
2020-10-22 22:09:31 -0300

    Enviador de emails con nodemailer

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 
 const express = require('express')
 
+const nodemailer = require("nodemailer");
+
 
 
 const port = 1000
+
+
+const miniOutlook = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'mckenna.langworth@ethereal.email',
+        pass: '93SvDHvMSEZKq2xPnV'
+    }
+});
 
 
 const app = express()
@@ -28,9 +40,19 @@ app.get("/contacto", function(request, response){  //anatomia de como crear ruta
 
  //plantilla modelo para "endpoints" de express()
 app.post("/enviar",function(request, response){ //<<<-------- anatomia modelo de como crear rutas en mi servidor con express. Esta el tipo de peticion y la ruta con la cual se va a acceder al codigo
-    const contacto = request.body
-    console.log(contacto)
-    response.end('desde aca vamos a hacer algo loco')
+    const contacto = request.body 
+
+    
+    miniOutlook.sendMail({
+        from: contacto.correo, // sender address
+        to: "ralsogaray91@hotmail.com", 
+        subject: contacto.asunto, // Subject line
+        //text: "Hello world?", // plain text body
+        html: `<blockquote>${contacto.mensaje}</blockquote>`, // html body
+    })
+
+
+    response.end('desde aca vamos a enviar mail de contacto')
 })
 
 
