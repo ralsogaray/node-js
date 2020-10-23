788ef021aa5f92391baf91e9f1d16451bc05f2e2
rodrigo alsogaray <rodrigoalsogaray@MacBook-Air-de-rodrigo.local>
2020-10-22 21:44:18 -0300

    prototipo del back end

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.listen(port)
 
 app.use( express.static( 'public' ) ) // con esto le digo q primero busque en la carpeta public y si no encuentra lo q busca ahi, buscara en app.js
 
+app.use( express.urlencoded({ extended : true}) ) //PROCESO LOS DATOS ENVIADOS EN URLENCODED y los transformo en un objeto 
+
 /* //plantilla modelo para "endpoints" de express()
 app.TIPO_HTTP("/ruta",function(request, response){ <<<-------- //anatomia modelo de como crear rutas en mi servidor con express. Esta el tipo de peticion y la ruta con la cual se va a acceder al codigo
 
@@ -26,6 +28,8 @@ app.get("/contacto", function(request, response){  //anatomia de como crear ruta
 
  //plantilla modelo para "endpoints" de express()
 app.post("/enviar",function(request, response){ //<<<-------- anatomia modelo de como crear rutas en mi servidor con express. Esta el tipo de peticion y la ruta con la cual se va a acceder al codigo
+    const contacto = request.body
+    console.log(contacto)
     response.end('desde aca vamos a hacer algo loco')
 })
 
