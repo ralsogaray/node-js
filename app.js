c67420965eccc0cf67dd505943451af419b7c9d6
rodrigo alsogaray <rodrigoalsogaray@MacBook-Air-de-rodrigo.local>
2020-10-27 21:25:50 -0300

    Incorporando dotenv para variables de entorno

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-
+//require('dotenv').config()
 const express = require('express')
-
-const nodemailer = require("nodemailer");
-
-
-
+const nodemailer = require("nodemailer"); //luego de instalar el module de nodemailer, lo llamo y deposito en la constante
 const port = 1000
 
 
-const miniOutlook = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
+const miniOutlook = nodemailer.createTransport({ //se configura quien va a hacer el envío de los datos pòr mail
+    host: process.env.HOST_MAIL, 
+    port: process.env.PUERTO_MAIL,
     auth: {
-        user: 'mckenna.langworth@ethereal.email',
-        pass: '93SvDHvMSEZKq2xPnV'
+        user: process.env.CASILLA_MAIL,
+        pass: process.env.CLAVE_MAIL
     }
 });
+/*
+const miniOutlook = nodemailer.createTransport({ //se configura quien va a hacer el envío de los datos pòr mail
+    service: 'gmail',
+    auth: {
+        user: 'maildeprueba3107@gmail.com',
+        pass: 'eant1234'
+    }
+});*/
 
 
 const app = express()
@@ -28,31 +32,29 @@ app.use( express.urlencoded({ extended : true}) ) //PROCESO LOS DATOS ENVIADOS E
 
 /* //plantilla modelo para "endpoints" de express()
 app.TIPO_HTTP("/ruta",function(request, response){ <<<-------- //anatomia modelo de como crear rutas en mi servidor con express. Esta el tipo de peticion y la ruta con la cual se va a acceder al codigo
-
 })*/
 
 app.get("/contacto", function(request, response){  //anatomia de como crear rutas en mi servidor con express. Esta el tipo de peticion y la ruta con la cual se va a acceder al codigo
     //response.end(`DESDE ACA VAMOS A CONTACTARNOS`)
 
-
 })
 
 
- //plantilla modelo para "endpoints" de express()
-app.post("/enviar",function(request, response){ //<<<-------- anatomia modelo de como crear rutas en mi servidor con express. Esta el tipo de peticion y la ruta con la cual se va a acceder al codigo
+
+app.post("/enviar",(request, response) => { //<<<-------- anatomia modelo de como crear rutas en mi servidor con express. Esta el tipo de peticion y la ruta con la cual se va a acceder al codigo
+    
     const contacto = request.body 
 
-    
     miniOutlook.sendMail({
-        from: contacto.correo, // sender address
-        to: "ralsogaray91@hotmail.com", 
-        subject: contacto.asunto, // Subject line
-        //text: "Hello world?", // plain text body
-        html: `<blockquote>${contacto.mensaje}</blockquote>`, // html body
+        from: contacto.correo, // sender address --> contacto.correo es por **** 
+        to: "rodrigo.alsogaray91@gmail.com",  // quien recibe
+        replyTo: contacto.correo,
+        subject: `Asunto ${contacto.asunto}, mail de contacto -> ${contacto.correo}`, // Subject line
+        //text: "Hello world?", // plain text body --> para enviar en modo texto plano..
+        html: `<blockquote>${contacto.mensaje}</blockquote>`, // html body --> para enviar en formato HTML
     })
 
-
-    response.end('desde aca vamos a enviar mail de contacto')
+    response.end('correo electrónico enviadp, Sr. Admin.')
 })
 
 
---
+++ test.js
@@ -0,0 +1,4 @@
+require('dotenv').config()
+
+console.log(process.env.CLAVE_MAIL)
+console.log(process.env.PUERTO_EMAIL)
